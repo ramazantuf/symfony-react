72fbaa18082e593b82fc767a7e42fe6620a00542
ramazantufekci <ramazan.tufekci@outlook.com>
2021-06-03 11:36:07 +0300

    Görev oluşturma api üzerinden

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TodoRepository;
+use App\Entity\Todo;
 
 /**
      * @Route("/api/todo", name="todo.")
@@ -25,5 +27,26 @@ class TodoController extends AbstractController
 	   }
 	   
 	   return $this->json($arrayOfTodos);
+    }
+	
+	/**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request): Response
+    {
+		
+       $content = json_decode($request->getContent());
+	   $todo = new Todo();
+	   $todo->setName($content->name);
+	   try{
+		   $em = $this->getDoctrine()->getManager();
+		   $em->persist($todo);
+		   $em->flush();
+		   return $this->json([
+				'todo' => $todo->toArray(),
+		   ]);
+	   }catch(Exception $exception){
+		   //return 
+	   }
     }
 }
